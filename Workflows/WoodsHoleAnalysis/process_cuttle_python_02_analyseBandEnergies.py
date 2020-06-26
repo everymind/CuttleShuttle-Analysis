edae51049b49f22a9305c60f13ec6ec79f202bf4
taunsquared <danbee@alum.mit.edu>
2020-06-26 13:15:50 +0100

    fixed finding first frame of pointwise significance

--- Workflows/WoodsHoleAnalysis/process_cuttle_python_02_analyseBandEnergies.py
+++ Workflows/WoodsHoleAnalysis/process_cuttle_python_02_analyseBandEnergies.py
@@ -800,8 +800,8 @@ for freq_band in Observed_DiffMeans_allFreq.keys():
     if freq_band in firstFrame_globalP005sig.keys() and firstFrame_globalP005sig[freq_band] != len(Observed_DiffMeans_allFreq[freq_band])-1:
         backwards_from_first_global_crossing = np.flip(range(firstFrame_globalP005sig[freq_band]))
         for frame in backwards_from_first_global_crossing:
-            if Observed_DiffMeans_allFreq[freq_band][frame]>pw005sig_UB[freq_band][frame] or Observed_DiffMeans_allFreq[freq_band][frame]<pw005sig_LB[freq_band][frame]:
-                firstFrame_P005sig[freq_band] = frame
+            if Observed_DiffMeans_allFreq[freq_band][frame]<pw005sig_UB[freq_band][frame] or Observed_DiffMeans_allFreq[freq_band][frame]>pw005sig_LB[freq_band][frame]:
+                firstFrame_P005sig[freq_band] = frame + 1
                 break
     else:
         firstFrame_P005sig[freq_band] = None
@@ -815,8 +815,8 @@ for freq_band in Observed_DiffMeans_ZSess_allFreq.keys():
     if freq_band in firstFrame_ZSess_globalP005sig.keys() and firstFrame_ZSess_globalP005sig[freq_band] != len(Observed_DiffMeans_allFreq[freq_band])-1:
         backwards_from_first_global_crossing = np.flip(range(firstFrame_ZSess_globalP005sig[freq_band]))
         for frame in backwards_from_first_global_crossing:
-            if Observed_DiffMeans_allFreq[freq_band][frame]>pw005sig_Zsess_UB[freq_band][frame] or Observed_DiffMeans_allFreq[freq_band][frame]<pw005sig_Zsess_LB[freq_band][frame]:
-                firstFrame_ZSess_P005sig[freq_band] = frame
+            if Observed_DiffMeans_allFreq[freq_band][frame]<pw005sig_Zsess_UB[freq_band][frame] or Observed_DiffMeans_allFreq[freq_band][frame]>pw005sig_Zsess_LB[freq_band][frame]:
+                firstFrame_ZSess_P005sig[freq_band] = frame + 1
                 break
     else:
         firstFrame_ZSess_P005sig[freq_band] = None
